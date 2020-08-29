960da3888873fd7c9ce0c26c79553fde781ea3cd
869339820 <869339820@qq.com>
2020-08-29 21:26:56 +0800

    更新版本到雷姆版啦！！！

--- app/src/main/java/com/zc/wake/activity/NetWakeActivity.java
+++ app/src/main/java/com/zc/wake/activity/NetWakeActivity.java
@@ -1,11 +1,18 @@
 package com.zc.wake.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.blankj.utilcode.util.BarUtils;
+import com.blankj.utilcode.util.ColorUtils;
 import com.blankj.utilcode.util.SPUtils;
 import com.dynamitechetan.flowinggradient.FlowingGradientClass;
 import com.zc.wake.R;
@@ -18,21 +25,19 @@ import pl.droidsonroids.gif.GifImageView;
 public class NetWakeActivity extends Activity implements View.OnLongClickListener {
     public static final  String MY_MAC ="mac";
     private BaseMacPopup baseMacPopup;
-
+    int mAlpha = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net_wake);
         RelativeLayout rl = findViewById(R.id.rl);
+        ImageView image_bg = findViewById(R.id.image_bg);
         GifImageView gifImageView = findViewById(R.id.gif);
         gifImageView.setOnLongClickListener(this);
         FlowingGradientClass grad = new FlowingGradientClass();
+        BarUtils.transparentStatusBar(this);
+        BarUtils.setStatusBarLightMode(this,false);
 
-                //translate
-        grad.setBackgroundResource(R.drawable.trans)
-                .onRelativeLayout(rl)
-                .setTransitionDuration(1000)
-                .start();
         baseMacPopup = new BaseMacPopup(this);
 
     }
